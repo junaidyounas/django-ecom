4336225471de8d34ef09ae18db9d17cfa79aac76
Junaid Younas <Junaid.ammar2013@gmail.com>
2023-10-02 00:18:12 +0500

    order detail page

--- store/views.py
+++ store/views.py
@@ -7,8 +7,8 @@ from random import sample
 def home(request):
     products = Product.objects.all()
     categories = Category.objects.all() 
-
     return render(request, 'home.html', {'products': products, 'categories': categories})
+
 
 def product(request, product_slug):
     all_products = list(Product.objects.exclude(slug=product_slug).order_by('?')[:4])
@@ -18,8 +18,13 @@ def product(request, product_slug):
         product = None
     return render(request, 'product.html', {'product': product, 'featured_products': all_products})
 
+
 def category_products(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
     return render(request, 'category_products.html', {'category': category, 'products': products})
+
+
+def order(request, product_slug):
+    return render(request, 'order.html')
 
